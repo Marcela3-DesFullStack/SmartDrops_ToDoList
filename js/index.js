8cdffa353ee978b1a595c82e52fbfaa20c2a0901
Marcela3 <148433020+Marcela3-DesFullStack@users.noreply.github.com>
2023-12-17 16:05:39 +0100

    Addition task finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 const date = document.querySelector(#date)
-const list = document.querySelector(#tasklist)
+const taskList = document.querySelector(#tasklist)
 const input = document.querySelector(#input)
-const input = document.querySelector(#addtask)
+const addTask = document.querySelector(#addtask)
+
+// Adding tasks//
+function addTask (task) {
+    const elementTask = `<i class="fa-sharp fa-regular fa-circle" style="color: #f1f3f8;"></i>
+                        <p class="text">${task}</p>
+                        <i id="readmore.btn" class="fa-solid fa-circle-plus" style="color: #f1f3f8;"></i>
+                        <i class="fa-solid fa-trash-can" style="color: #f1f3f8;"></i>  
+                    `
+    tasklist.insertAdjacentHTML("beforend", elementTask);
+}
+addTask.addEventListener('click',()=> {
+    const task = input.value
+    if(task) {
+        addTask(task)
+    }
+    input.value=""
+})
