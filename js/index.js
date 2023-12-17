6680b56197d77273ea278626fad859dbb9caa8cc
Marcela3 <148433020+Marcela3-DesFullStack@users.noreply.github.com>
2023-12-17 18:36:52 +0100

    Finished addition task

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
-const date = document.querySelector(#date)
-const taskList = document.querySelector(#tasklist)
-const input = document.querySelector(#input)
-const addTask = document.querySelector(#addtask)
+const date = document.querySelector("#date")
+const taskList = document.querySelector("#tasklist")
+const input = document.querySelector("#input")
+const taskEnter = document.querySelector("#task-enter")
 
 // Adding tasks//
 function addTask (task) {
-    const elementTask = `<i class="fa-sharp fa-regular fa-circle" style="color: #f1f3f8;"></i>
+    const elementTask = `<li id="elementotask">
+                        <i class="fa-sharp fa-regular fa-circle" style="color: #f1f3f8;"></i>
                         <p class="text">${task}</p>
                         <i id="readmore.btn" class="fa-solid fa-circle-plus" style="color: #f1f3f8;"></i>
-                        <i class="fa-solid fa-trash-can" style="color: #f1f3f8;"></i>  
+                        <i class="fa-solid fa-trash-can" style="color: #f1f3f8;"></i> 
+                        </li>  
                     `
-    tasklist.insertAdjacentHTML("beforend", elementTask);
+    tasklist.insertAdjacentHTML("beforeend", elementTask);
 }
-addTask.addEventListener('click',()=> {
+taskEnter.addEventListener('click',()=> {
     const task = input.value
     if(task) {
         addTask(task)
     }
-    input.value=""
+    input.value= ''
+
 })
