c90e2a1f9822eaafe03efda8d437628e290d3a0d
Marcela3 <148433020+Marcela3-DesFullStack@users.noreply.github.com>
2023-12-17 14:29:57 +0100

    Addition variables into js

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const date = document.querySelector(#date)
+const list = document.querySelector(#tasklist)
+const input = document.querySelector(#input)
+const input = document.querySelector(#addtask)
